40aa01eb779743059ec2f374bd52b1d70ed5d49a
Farshad <farshad.ghanbari@hotmail.com>
2021-09-03 10:22:06 +0800

    Changed from static routing to dynamic

--- vis_man/survey/models.py
+++ vis_man/survey/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Site(models.Model):
+  name = models.CharField(max_length=50)
+  address = models.CharField(max_length=100)
+
+  def __str__(self):
+    return self.name
