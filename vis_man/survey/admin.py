42847d8c97cd59041b4439ce5432727726831858
gauravchakraverty <gauravchakraverty@gmail.com>
2021-09-03 18:23:07 +0800

    Edited the model and DB for visitor forms.

--- vis_man/survey/admin.py
+++ vis_man/survey/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Site
+from .models import Site, Visitor
 
 admin.site.register(Site)
+admin.site.register(Visitor)
 
--- vis_man/survey/models.py
+++ vis_man/survey/models.py
@@ -1,25 +1,28 @@
 from django.db import models
 
 class Site(models.Model):
-  name = models.CharField(max_length=50)
+  name = models.CharField(max_length=50, unique=True)
   address = models.CharField(max_length=100)
 
   def __str__(self):
     return self.name
 
 
-class Form(models.Model):
-  name = models.CharField(max_length=50)
+class Visitor(models.Model):
+  visitor_name = models.CharField(max_length=50)
   role = models.CharField(max_length=50)
   email = models.EmailField()
-  phone = models.IntegerField()
+#   phone = models.IntegerField()
   nightstay = models.BooleanField(default=False)
-  arrival = models.DateField()
-  departure = models.DateField()
-  induction = models.CharField(max_length=50)
-  emergency_name = models.CharField(max_length=50)
-  emergency_phone = models.IntegerField()
-  emergency_relation = models.CharField(max_length=50)
+  checkin = models.DateTimeField()
+  planned_checkout = models.DateTimeField()
+  checkout = models.DateTimeField()
+#   induction = models.CharField(max_length=50)
+#   emergency_name = models.CharField(max_length=50)
+#   emergency_phone = models.IntegerField()
+#   emergency_relation = models.CharField(max_length=50)
+#   place = models.ForeignKey('Site',on_delete=models.PROTECT, default=1)
+  
 
   def __str__(self):
     return self.name
