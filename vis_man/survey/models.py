3b1c8694d003e0d516c3995f8eba9fd8502573a1
gauravchakraverty <gauravchakraverty@gmail.com>
2021-09-03 16:16:24 +0800

    Made a model for a general form and tested migration.

--- vis_man/survey/models.py
+++ vis_man/survey/models.py
@@ -6,3 +6,20 @@ class Site(models.Model):
 
   def __str__(self):
     return self.name
+
+
+class Form(models.Model):
+  name = models.CharField(max_length=50)
+  role = models.CharField(max_length=50)
+  email = models.EmailField()
+  phone = models.IntegerField()
+  nightstay = models.BooleanField(default=False)
+  arrival = models.DateField()
+  departure = models.DateField()
+  induction = models.CharField(max_length=50)
+  emergency_name = models.CharField(max_length=50)
+  emergency_phone = models.IntegerField()
+  emergency_relation = models.CharField(max_length=50)
+
+  def __str__(self):
+    return self.name
