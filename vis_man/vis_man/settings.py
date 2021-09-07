4b644cb8a61b69fe54bb7a11ce9097620ec57d77
Finn-Mur98 <89336555+Finn-Mur98@users.noreply.github.com>
2021-09-07 14:15:42 +0800

    created admin app
    Created the admin app with some skeleton pages

--- vis_man/vis_man/settings.py
+++ vis_man/vis_man/settings.py
@@ -25,7 +25,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'survey.apps.SurveyConfig'
+    'survey.apps.SurveyConfig', 
+    'vis_man_admin.apps.VisManAdminConfig'
 ]
 
 MIDDLEWARE = [
@@ -94,9 +95,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'en-au'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Australia/Perth'
 
 USE_I18N = True
 
