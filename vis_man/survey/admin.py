5b118b87c6c48b6d1bd4ed6b0c6bb5624934ae1b
wambampham <22707439@student.uwa.edu.au>
2021-09-14 14:45:42 +0800

    changed the way users/checkout history is sorted and displayed in admin app

--- vis_man/survey/admin.py
+++ vis_man/survey/admin.py
@@ -1,7 +1,19 @@
 from django.contrib import admin
 from .models import Site, Visitor, History
 
-admin.site.register(Site)
-admin.site.register(Visitor)
-admin.site.register(History)
+# @admin.register(Site)
+# class PersonAdmin(admin.ModelAdmin):
+#     list_display = 
+
+@admin.register(Visitor)
+class PersonAdmin(admin.ModelAdmin):
+    list_display = ("last_name", "first_name")
+
+@admin.register(History)
+class PersonAdmin(admin.ModelAdmin):
+    list_display = ('visitor',"checkin", 'checkout', 'nightstay')
+
+# admin.site.register(Site)
+# admin.site.register(Visitor)
+# admin.site.register(History)
 
