156e9b4c2c9a446089f8feb097973be54b699c37
gauravchakraverty <gauravchakraverty@gmail.com>
2021-09-15 17:40:22 +0800

    Updated DB model. Fixed error msg.

--- vis_man/survey/models.py
+++ vis_man/survey/models.py
@@ -7,7 +7,7 @@ class Site(models.Model):
   id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
   name = models.CharField(max_length=100, unique=True)
   accomodation = models.BooleanField(default=False)
-  visitors = models.ManyToManyField('Visitor', null=True, blank=True)
+  visitors = models.ManyToManyField('Visitor', blank=True)
 
   def __str__(self):
     return self.name
