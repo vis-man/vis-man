8f1db7084d28ddd7d20af4db2c77d111f3274b9b
gauravchakraverty <gauravchakraverty@gmail.com>
2021-09-15 14:26:01 +0800

    Added Temp Role Dropdown above actual role textbox. Updated MainForm.

--- vis_man/survey/forms.py
+++ vis_man/survey/forms.py
@@ -5,8 +5,19 @@ from phonenumber_field.widgets import PhoneNumberPrefixWidget
 
 
 class MainForm(forms.ModelForm):
+    role_choices = (('staff', 'UWA Staff'),
+                    ('student', 'UWA Student'),
+                    ('other_staff', 'Other University staff'),
+                    ('other_student', 'Other University student'),
+                    ('contractor', 'Contractor'),
+                    ('other', 'Other'))
+
+    # role_dropdown = forms.Select()
+    role_dropdown = forms.CharField(label='Select Role', widget=forms.Select(choices=role_choices, attrs={'class': 'form-control'}))
+
     class Meta:
         model = Visitor
+
         fields = [
             'first_name',
             'last_name',
@@ -14,11 +25,11 @@ class MainForm(forms.ModelForm):
             'phone_number',
             'role',
             'nightstay',
-            'planned_checkout',
             'emergency_first_name',
             'emergency_last_name',
             'emergency_phone',
-            'emergency_relation'
+            'emergency_relation',
+            'planned_checkout'
         ]
 
         widgets = {
@@ -28,11 +39,11 @@ class MainForm(forms.ModelForm):
             'phone_number': PhoneNumberPrefixWidget(attrs={'class': 'form-control'}, initial='AU'),
             'role': forms.TextInput(attrs={'class': 'form-control'}),
             'nightstay': forms.CheckboxInput(attrs={'class': 'form-check-input'}),
-            'planned_checkout': forms.DateTimeInput(attrs={'type': 'datetime-local', 'class': 'form-control'}),
             'emergency_first_name': forms.TextInput(attrs={'class': 'form-control'}),
             'emergency_last_name': forms.TextInput(attrs={'class': 'form-control'}),
             'emergency_phone': PhoneNumberPrefixWidget(attrs={'class': 'form-control'}, initial='AU'),
-            'emergency_relation': forms.TextInput(attrs={'class': 'form-control'})
+            'emergency_relation': forms.TextInput(attrs={'class': 'form-control'}),
+            'planned_checkout': forms.DateTimeInput(attrs={'type': 'datetime-local', 'class': 'form-control'})
         }
 
         labels = {
@@ -40,17 +51,29 @@ class MainForm(forms.ModelForm):
             'last_name': 'Last Name',
             'email': 'Email',
             'phone_number': 'Phone Number',
-            'role': 'Role',
+            'role': '',
             'nightstay': 'Are you planning to stay overnight?',
-            'planned_checkout': 'Planned Checkout',
             'emergency_first_name': 'Emergency Contact First Name',
             'emergency_last_name': 'Emergency Contact last Name',
             'emergency_phone': 'Emergency Contact Number',
-            'emergency_relation': 'Relationship'
+            'emergency_relation': 'Relationship',
+            'planned_checkout': 'Planned Checkout',
         }
 
     def __init__(self, exclude, *args, **kwargs):
         super(MainForm, self).__init__(*args, **kwargs)
+        self.order_fields(['first_name',
+                       'last_name',
+                       'email',
+                       'phone_number',
+                       'role_dropdown',
+                       'role',
+                       'nightstay',
+                       'emergency_first_name',
+                       'emergency_last_name',
+                       'emergency_phone',
+                       'emergency_relation',
+                       'planned_checkout'])
         if not exclude['accomodation']:
             del self.fields['nightstay']
             del self.fields['emergency_first_name']
@@ -60,7 +83,6 @@ class MainForm(forms.ModelForm):
 
 
 class Signout(forms.Form):
-    email= forms.EmailField(label='Email')
-    phone_number= PhoneNumberField(label=('Phone Number'),required=True,widget=PhoneNumberPrefixWidget(attrs={'class': 'form-control'}, initial='AU'))
+    email = forms.EmailField(label='Email')
+    phone_number = PhoneNumberField(label=('Phone Number'), required=True, widget=PhoneNumberPrefixWidget(attrs={'class': 'form-control'}, initial='AU'))
     email.widget.attrs.update({'class': 'form-control'})
-   
--- vis_man/survey/static/survey/form.js
+++ vis_man/survey/static/survey/form.js
@@ -4,6 +4,10 @@ $("#id_emergency_last_name").parents('div').hide()
 $("#id_emergency_phone_0").parents('div').hide()
 $("#id_emergency_phone_1").parents('div').hide()
 $("#id_emergency_relation").parents('div').hide()
+// $("#id_role").parents('div').hide()
+document.getElementById("id_role").value = "Hello"
+// document.getElementById("role_dropdown").value
+console.log(document.getElementById("role_dropdown").value)
 
 $("#id_nightstay").click(function(){
     var checkBox = document.getElementById("id_nightstay");
@@ -22,3 +26,21 @@ $("#id_nightstay").click(function(){
         $("#id_emergency_relation").parents('div').hide()
     }
 })
+
+// $("#role_dropdown").change(function(){
+//     var role = document.getElementById("role_dropdown");
+//     if(role) {
+//         $("#id_emergency_first_name").parents('div').show()
+//         $("#id_emergency_last_name").parents('div').show()
+//         $("#id_emergency_phone_0").parents('div').show()
+//         $("#id_emergency_phone_1").parents('div').show()
+//         $("#id_emergency_relation").parents('div').show()
+//     } 
+//     if(checkBox.checked == false) {
+//         $("#id_emergency_first_name").parents('div').hide()
+//         $("#id_emergency_last_name").parents('div').hide()
+//         $("#id_emergency_phone_0").parents('div').hide()
+//         $("#id_emergency_phone_1").parents('div').hide()
+//         $("#id_emergency_relation").parents('div').hide()
+//     }
+// })
