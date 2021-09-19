d818b7cc2d98ac9dd66d7b20a2e8cab919c50d50
Allthat9999 <1076680533@qq.com>
2021-09-19 19:25:32 +0800

    Adding button which could export visitor data to CSV

--- vis_man/survey/admin.py
+++ vis_man/survey/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import Site, Visitor, History
-
+from django.http import HttpResponse
+import csv
 class AdminArea(admin.AdminSite):
   site_header = 'UWA Visitor Management System'
   index_title = 'Admin Dashboard'
@@ -23,6 +24,19 @@ class SiteAdmin(admin.ModelAdmin):
 
 @admin.register(Visitor, site=vms_admin)
 class VisitorAdmin(admin.ModelAdmin):
+  actions = ["export_as_csv"]
+  def export_as_csv(self, request, queryset):
+    meta = self.model._meta
+    field_names = [field.name for field in meta.fields]
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename={}.csv'.format(meta)
+    writer = csv.writer(response)
+    writer.writerow(field_names)
+    for obj in queryset:
+      row = writer.writerow([getattr(obj, field) for field in field_names])
+    return response
+  export_as_csv.short_description = "Export Selected"
+
   fieldsets = (
     ("Visitor's Contact Information",{
       'fields':(('first_name','last_name'),('email','phone_number'),'role')
@@ -48,7 +62,7 @@ class VisitorAdmin(admin.ModelAdmin):
     'emergency_relation'
   ]
   readonly_fields = ['checkin']
-  list_filter = ('role', 'checkout', 'nightstay')
+  list_filter = ('role', 'checkout', 'nightstay', 'checkin', 'planned_checkout')
   inlines = [HistoryInline]
 
   def visitor_name(self, obj):
