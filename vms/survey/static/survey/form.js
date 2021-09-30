78eb2bcc1714f6c05034de02b811a1a21bfa8614
gauravchakraverty <gauravchakraverty@gmail.com>
2021-09-30 18:13:56 +0800

    Implemented feature to detect if browser is Firefox and show DateTimePicker  accordingly.

--- vms/survey/static/survey/form.js
+++ vms/survey/static/survey/form.js
@@ -38,13 +38,26 @@ $("#id_role_dropdown").change(function(){
     
 })
 
-// for datepicker
-config = {
-    enableTime: true,
-    enableSeconds: true,
-    dateFormat: "Y-m-d H:i:s",
+// // for datepicker
+// config = {
+//     enableTime: true,
+//     enableSeconds: true,
+//     dateFormat: "Y-m-d H:i:s",
+// }
+// flatpickr("input[type=datetime-local]", config);
+
+let f = navigator.userAgent.search("Firefox");
+
+console.log(f > -1)
+
+if (f > -1) {
+    config = {
+        enableTime: true,
+        enableSeconds: true,
+        dateFormat: "Y-m-d H:i:s",
+    }
+    flatpickr("input[type=datetime-local]", config);
 }
-flatpickr("input[type=datetime-local]", config);
 
 
 
