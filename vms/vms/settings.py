9af4c188e3622f42c6fcd5ce9ff1fcfe3a08f215
gauravchakraverty <gauravchakraverty@gmail.com>
2021-10-05 18:19:00 +0800

    Fixed issue with checkin time not changing and removed timezoneAUS as it was causing wrong time to get recorded.

--- vms/vms/settings.py
+++ vms/vms/settings.py
@@ -88,15 +88,15 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-au'
+# LANGUAGE_CODE = 'en-au'
 
-TIME_ZONE = 'Australia/Perth'
+# TIME_ZONE = 'Australia/Perth'
 
-USE_I18N = True
+# USE_I18N = True
 
-USE_L10N = True
+# USE_L10N = True
 
-USE_TZ = True
+# USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
