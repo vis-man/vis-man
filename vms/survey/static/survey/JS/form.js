e571c727abd241369c8393fd3e58d6e1e488a9a7
gauravchakraverty <gauravchakraverty@gmail.com>
2021-10-07 22:54:13 +0800

    Shifted JS for success message back to form.js

--- vms/survey/static/survey/JS/form.js
+++ vms/survey/static/survey/JS/form.js
@@ -61,48 +61,26 @@ else if (s > -1) {
     flatpickr("input[type=datetime-local]", config);
 }
 
-// const Toast = Swal.mixin({
-//     toast: true,
-//     position: 'top',
-//     showConfirmButton: false,
-//     timer: 3000,
-//     timerProgressBar: true,
-//     didOpen: (toast) => {
-//         toast.addEventListener('mouseenter', Swal.stopTimer)
-//         toast.addEventListener('mouseleave', Swal.resumeTimer)
-//     }
-// })
+const Toast = Swal.mixin({
+    toast: true,
+    position: 'top',
+    showConfirmButton: false,
+    timer: 5000,
+    timerProgressBar: true,
+    didOpen: (toast) => {
+        toast.addEventListener('mouseenter', Swal.stopTimer)
+        toast.addEventListener('mouseleave', Swal.resumeTimer)
+    }
+})
 
-// Toast.fire({
-//     icon: 'success',
-//     title: 'Signed in successfully'
-//     })
+document.getElementById("out_form").addEventListener("submit", myFunction2);
 
-// $("#in_form").onsubmit = function() {myFunction()}
-// $("#out_form").onsubmit = function() {myFunction()}
-// function myFunction() {
-//     console.log("HELOOOOOOOOOOOOOOOOOOOOOOOOOOOO");
-//     alert("The form was submitted");
-//   }
-
-
-
-// $("#in_form").onsubmit(function(){
-//     console.log("HELOOOOOOOOOOOOOOOOOOOOOOOOOOOO");
-//     Toast.fire({
-//         icon: 'success',
-//         title: 'Signed in successfully'
-//       })
-// })
-
-
-
-// document.getElementById("#in_form").addEventListener("submit", myFunction2);
-// document.getElementById("#out_form").addEventListener("submit", myFunction2);
-
-// function myFunction2() {
-//     alert("The form was submitted");
-//     console.log("HELOOOOOOOOOOOOOOOOOOOOOOOOOOOO");
-// }
+function myFunction2() {
+    console.log("HELOOOOOOOOOOOOOOOOOOOOOOOOOOOO");
+    Toast.fire({
+    icon: 'success',
+    title: 'Signed in successfully'
+    })
+}
 
 
