94468ed0dba559222be313b9e7f37fe5fbbe36de
wambampham <22707439@student.uwa.edu.au>
2021-10-14 00:29:33 +0800

    django custom management command script to clear 3y.o data, can test by typing 'py manage.py datadel', to be set up as a scheduled task on cron or task scheduler, will include how on documentation as its the simplest way

--- vms/survey/management/commands/datadel.py
+++ vms/survey/management/commands/datadel.py
@@ -1,5 +1,5 @@
 from django.core.management.base import BaseCommand
-from django.db.models import Count
+from django.db.models import Q
 from survey.models import Visitor, History
 from datetime import timedelta, datetime
   
@@ -7,15 +7,15 @@ def now():
     return datetime.now()
   
 class Command(BaseCommand):
-    help = 'Displays stats related to Article and Comment models'
+    help = 'Removes visitors and histories older than 3 years (1095 days)'
   
     def handle(self, *args, **kwargs):
-        From = now() - timedelta(minutes=2)
+        From = now() - timedelta(days=1095)
         To = now()
   
-        articles_published_in_last_5_hour = Visitor.objects.filter(
-            checkin__gt=From, checkin__lte=To).count()
-
-        print("Articles Published in last 2 min = ",
-              articles_published_in_last_5_hour)
+        Visitor.objects.filter(~Q(checkin__gt=From, checkin__lte=To)).delete()
+        History.objects.filter(~Q(checkin__gt=From, checkin__lte=To)).delete()
         
+
+        
+    
