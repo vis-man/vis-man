0441e92fea09407053aa2ae4e95ac0cce4cbfe3c
gauravchakraverty <gauravchakraverty@gmail.com>
2021-10-14 20:50:48 +0800

    Fixed issue with calendar and roles not showing for certain sites. And roles getting hidden on page refresh.

--- vms/survey/static/survey/JS/form.js
+++ vms/survey/static/survey/JS/form.js
@@ -2,32 +2,9 @@
 $("#id_role").parents('div').hide()
 
 var checkBox = document.getElementById("id_nightstay");
-if (checkBox.checked === false) {
-    $("#id_emergency_first_name").parents('div').hide()
-    $("#id_emergency_last_name").parents('div').hide()
-    $("#id_emergency_phone").parents('div').hide()
-    $("#id_emergency_relation").parents('div').hide()
-}
-
 var chosen_role = document.getElementById("id_role_dropdown")
 var role_text = chosen_role.options[chosen_role.selectedIndex].text
 document.getElementById("id_role").value = role_text
-
-$("#id_nightstay").click(function () {
-    var checkBox = document.getElementById("id_nightstay");
-    if (checkBox.checked === true) {
-        $("#id_emergency_first_name").parents('div').show()
-        $("#id_emergency_last_name").parents('div').show()
-        $("#id_emergency_phone").parents('div').show()
-        $("#id_emergency_relation").parents('div').show()
-    }
-    if (checkBox.checked === false) {
-        $("#id_emergency_first_name").parents('div').hide()
-        $("#id_emergency_last_name").parents('div').hide()
-        $("#id_emergency_phone").parents('div').hide()
-        $("#id_emergency_relation").parents('div').hide()
-    }
-})
 
 $("#id_role_dropdown").change(function () {
     role_text = chosen_role.options[chosen_role.selectedIndex].text
@@ -78,6 +55,34 @@ else if (s > -1) {
     flatpickr("input[type=datetime-local]", config);
 }
 
+$("#id_nightstay").click(function () {
+    var checkBox = document.getElementById("id_nightstay");
+    if (checkBox.checked === true) {
+        $("#id_emergency_first_name").parents('div').show()
+        $("#id_emergency_last_name").parents('div').show()
+        $("#id_emergency_phone").parents('div').show()
+        $("#id_emergency_relation").parents('div').show()
+    }
+    if (checkBox.checked === false) {
+        $("#id_emergency_first_name").parents('div').hide()
+        $("#id_emergency_last_name").parents('div').hide()
+        $("#id_emergency_phone").parents('div').hide()
+        $("#id_emergency_relation").parents('div').hide()
+    }
+})
+
+if (chosen_role.value === 'other') {
+    $("#id_role").parents('div').show()
+    document.getElementById("id_role").value = ''
+    document.getElementById("id_role").placeholder = 'Please Key in Role'
+}
+
+if (checkBox.checked === false) {
+    $("#id_emergency_first_name").parents('div').hide()
+    $("#id_emergency_last_name").parents('div').hide()
+    $("#id_emergency_phone").parents('div').hide()
+    $("#id_emergency_relation").parents('div').hide()
+}
 
 
 
